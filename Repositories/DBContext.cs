e79b3dd1842adad2c83cf381baa51f00ee67ef4c
José Florindo Sgorlon Filho <jsgorlon@gmail.com>
2022-05-08 22:19:08 -0300

    Chore: Inclusão SqlClient

--- Repositories/DBContext.cs
+++ Repositories/DBContext.cs
@@ -4,23 +4,21 @@ namespace logiWeb.Repositories
 {
     public abstract class DBContext
     {
-        //TODO
-        // private readonly string strConn = @"Data Source=DESKTOP-BP1KIU0\SQLSERVE01;
-        //  Initial Catalog=BDEcommerce;
-        //  Integrated Security=true";
-        // User Id=sa; Password=F4tecSQL!
+        private readonly string strConn = @"Data Source=DESKTOP-BP1KIU0\SQLSERVE01;
+         Initial Catalog=BDEcommerce;
+         Integrated Security=true";
 
-        //  protected SqlConnection connection;
+        protected SqlConnection connection;
 
-        //  public DBContext()
-        //  {
-        //      connection = new SqlConnection(strConn);
-        //      connection.Open();
-        //  }
+        public DBContext()
+        {
+            connection = new SqlConnection(strConn);
+            connection.Open();
+        }
 
-        //  public void Dispose()
-        //  {
-        //      connection.Close();
-        //  }
+        public void Dispose()
+        {
+            connection.Close();
+        }
     }
 }
