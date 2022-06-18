0d39e886ed2d2242590904126475bfbc9d0d5c13
fuzz <allandiego252@gmail.com>
2022-06-18 19:53:16 -0300

    View de entrega

--- wwwroot/js/events/entrega.js
+++ wwwroot/js/events/entrega.js
@@ -2,6 +2,27 @@
 
 $(document).ready(_=>{
 
+    $("table").bootstrapTable(); 
+
+    dialogEntrega = $.dialog({
+        title: `<span style="font-size:18px !important;" class="fw-bold">Nova entrega</span>`,
+        content: $("#template_cad_entrega").html(), 
+        type: 'green', 
+        columnClass: 'col-12 col-md-7 col-lg-6', 
+        draggable: false, 
+        closeIcon: true, 
+        onOpen: function(){
+
+        }, 
+        onOpenBefore: function(){
+            $("table").bootstrapTable();
+        },
+        criar: function() {
+            this.buttons = {}; 
+            this.open(); 
+        }
+    });
 
 
+    $("#btNovaEntrega").click(_=>dialogEntrega.criar());
 });
